b2c0203a30dd49fe3b6e3a26374665d19a76d136
t-guan <56769857+t-guan@users.noreply.github.com>
2022-03-04 12:51:09 -0800

    Added increased functionality with user input
    Can fully collect data with regards to the panda framework.
    Resolved queue issue with get_notwait() because the empty queue was waiting for a value within the queue.
    TODO: Complete user implementation for collection of data over a large span, maybe as one whole csv
    Resolve timing issue with data collection

--- PythonBeeHiveGasSensor/GasSensorInterface.py
+++ PythonBeeHiveGasSensor/GasSensorInterface.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Mar  2 15:46:16 2022
+Collects data via serialport and writes to CSV, can collect based on specific
+timeframe
 
 @author: Thomas
 """
@@ -8,8 +10,6 @@ Created on Wed Mar  2 15:46:16 2022
 import serial;
 import time;
 import csv;
-import matplotlib.pyplot as plt
-import numpy as np
 from queue import Queue
 
 #Initialize serial
@@ -23,21 +23,53 @@ TwoData=Queue(maxsize=0)
 TwentyData=Queue(maxsize=0)
 EightData=Queue(maxsize=0)
 #Change the time value as required
+#Currently gives 6 full rows of data and one row of garbage values
+#Time delay after every sent packet is 500ms on Arduino End
+#May need to calc for complexity for more accurate results
 t_end=time.time()+5
-
-
 while time.time()<t_end:
     data=arduino.readline()
     decoded_bytes = float(data[0:len(data)-2].decode("utf-8"))
     dataQueue.put(decoded_bytes)
 arduino.close()
-
+with open("test_data.csv","a", newline='') as f:
+                writer = csv.writer(f,delimiter=",")
+                writer.writerow(["TGS2600","TGS2603","TGS2602","TGS2620","TGS832A"])
 while(dataQueue.empty()==False):
-    val=dataQueue.get()
-    if (val==255.0):
+    try:
+        val=dataQueue.get_nowait()
+        if (val==255.0):
+            try:
+                zeroval=dataQueue.get_nowait()
+            except:
+                zeroval=0
+            try:
+                threeval=dataQueue.get_nowait()
+            except:
+                threeval=0
+            try:
+                twoval=dataQueue.get_nowait()
+            except:
+                twoval=0
+            try:
+                twentyval=dataQueue.get_nowait()
+            except:
+                twentyval=0
+            try:
+                eightval=dataQueue.get_nowait()
+            except:
+                eightval=0
             with open("test_data.csv","a", newline='') as f:
                 writer = csv.writer(f,delimiter=",")
-                writer.writerow([dataQueue.get(),dataQueue.get(),dataQueue.get(),dataQueue.get(),dataQueue.get()])
+                writer.writerow([zeroval,threeval,twoval,twentyval,eightval])
+            ZeroData.put(zeroval)
+            ThreeData.put(threeval)
+            TwoData.put(twoval)
+            TwentyData.put(twentyval)
+            EightData.put(eightval)
+    except:
+        print("No more items in Queue")
+
 print("Done!")
         
     
