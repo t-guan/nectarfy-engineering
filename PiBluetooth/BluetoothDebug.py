e86332158a247bfbd6637ec624cb1de057d197a8
t-guan <thomas.s.guan@gmail.com>
2022-03-16 16:38:10 -0700

    Updated Bluetooth Debug to differenate between different button presses

--- PiBluetooth/BluetoothDebug.py
+++ PiBluetooth/BluetoothDebug.py
@@ -11,11 +11,13 @@ bd[4,0].color="red"
 #Define Presssed Function to catch any button press
 def pressed(pos):
     print("Button {}.{} pressed".format(pos.col,pos.row))
+def press_1(pos):
+    print("BLOOOOOO")
+def press_2(pos):
+    print("GREEEEEEEEEEE")
+def press_3(pos):
+    print("REEEEEEEEEEEEED")
 while True:
-   if(bd[0,0].when_pressed()):
-       print("BLOOO")
-   elif(bd[2,0].when_pressed()):
-       print("GREEE")
-   elif(bd[4,0].when_pressed()):
-       print("REDDDDD")
-    
+    bd[0,0].when_pressed=press_1
+    bd[2,0].when_pressed=press_2
+    bd[4,0].when_pressed=press_3
